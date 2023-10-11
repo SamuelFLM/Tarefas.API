82e1213b953dfe1ab3d004645c5b0d1d6b7e982f
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-10-11 18:25:25 -0300

    add: patter repository

--- Task.API/Persistence/Repositories/ITaskRepository.cs
+++ Task.API/Persistence/Repositories/ITaskRepository.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Task.API.Entities;
 
 namespace Task.API.Persistence.Repositories
 {
     public interface ITaskRepository
     {
-        
+        List<Tasks> GetAll();
+        Tasks GetById(int id);
+        void Post(Tasks tasks);
+        void Put(Tasks tasks);
+        void Delete(Tasks tasks);
     }
 }
--- Task.API/Persistence/Repositories/TaskRepository.cs
+++ Task.API/Persistence/Repositories/TaskRepository.cs
@@ -2,11 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Task.API.Entities;
 
 namespace Task.API.Persistence.Repositories
 {
-    public class TaskRepository
+    public class TaskRepository : ITaskRepository
     {
-        
+        private readonly TaskContext _context;
+
+        public TaskRepository(TaskContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(Tasks tasks)
+        {
+            _context.Requests.Remove(tasks);
+            _context.SaveChanges();
+        }
+
+        public List<Tasks> GetAll()
+        {
+            return _context.Requests.AsNoTracking().ToList();
+        }
+
+        public Tasks GetById(int id)
+        {
+            return _context.Requests.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void Post(Tasks tasks)
+        {
+            _context.Requests.Add(tasks);
+            _context.SaveChanges();
+        }
+
+        public void Put(Tasks tasks)
+        {
+            _context.Requests.Update(tasks);
+            _context.SaveChanges();
+        }
     }
 }
