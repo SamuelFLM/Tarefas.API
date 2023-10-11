f399ed6f49de271ff16d92d482405ec9394c11d9
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-10-11 18:15:24 -0300

    add

--- Task.API/Persistence/TaskContext.cs
+++ Task.API/Persistence/TaskContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Task.API.Entities;
 
 namespace Task.API.Persistence
 {
     public class TaskContext : DbContext
     {
-        
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+
+        public DbSet<Tasks> Requests { get; set; }
     }
 }
